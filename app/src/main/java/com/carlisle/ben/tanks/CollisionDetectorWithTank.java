93f688b5d9679ef89630d4f898e654fc84d4c35d
Ramandeep Singh <ramandeep.singh.us@gmail.com>
2018-04-07 15:04:19 -0400

    updated collision detector

--- app/src/main/java/com/carlisle/ben/tanks/CollisionDetectorWithTank.java
+++ app/src/main/java/com/carlisle/ben/tanks/CollisionDetectorWithTank.java
@@ -2,16 +2,14 @@ package com.carlisle.ben.tanks;
 
 public class CollisionDetectorWithTank {
 
-    private int[] otherPos;
-    private int[][] vertices = new int[3][2];
-    private double angle;
+    private int[] entityVertices = new int[2];
+    private int[][] tankVertices = new int[3][2];
     private double aDet;
     private double bDet;
 
-    public CollisionDetectorWithTank(int[][] vertices, int[] otherPos, double angle) {
-        this.otherPos = otherPos;
-        this.angle = angle;
-        this.vertices = vertices;
+    public CollisionDetectorWithTank(int[][] tankVertices, int[] entityVertices) {
+        this.tankVertices = tankVertices;
+        this.entityVertices = entityVertices;
     }
 
     public boolean isCollision() {
@@ -27,11 +25,13 @@ public class CollisionDetectorWithTank {
 
     public void setCalculatedDeterminants() {
 
-        aDet = findDeterminant(otherPos, vertices[2]) - findDeterminant(vertices[0], vertices[2]);
-        aDet /= findDeterminant(vertices[1], vertices[2]);
+        int[] coordinates = {entityVertices[0], entityVertices[1]};
 
-        bDet = findDeterminant(otherPos, vertices[1]) - findDeterminant(vertices[0], vertices[1]);
-        bDet /= findDeterminant(vertices[1], vertices[2]);
+        aDet = findDeterminant(coordinates, tankVertices[2]) - findDeterminant(tankVertices[0], tankVertices[2]);
+        aDet /= findDeterminant(tankVertices[1], tankVertices[2]);
+
+        bDet = findDeterminant(coordinates, tankVertices[1]) - findDeterminant(tankVertices[0], tankVertices[1]);
+        bDet /= findDeterminant(tankVertices[1], tankVertices[2]);
     }
 
     public double findDeterminant(int[] first, int[] second) {
