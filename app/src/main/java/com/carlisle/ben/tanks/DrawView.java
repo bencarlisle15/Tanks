50d5d8a6a0c7310fe71dcf79e7bc174f51f4ecc0
bencarlisle15 <bencarlisle15@gmail.com>
2018-04-07 18:59:40 -0400

    Added fucking movement

--- app/src/main/java/com/carlisle/ben/tanks/DrawView.java
+++ app/src/main/java/com/carlisle/ben/tanks/DrawView.java
@@ -46,7 +46,7 @@ public class DrawView extends View {
 				{
 					if(entity instanceof Tank)
 					{
-						int[][] vertices = ((Tank) entity).getVertices(entity.getXpos(), entity.getYpos(), ((Tank) entity).getJoyAngle());
+						int[][] vertices = ((Tank) entity).getVertices(entity.getXpos(), entity.getYpos());
 //						Log.e(String.valueOf(vertices[0][0]), String.valueOf(vertices[0][1]));
 //						Log.e(String.valueOf(vertices[1][0]), String.valueOf(vertices[1][1]));
 //						Log.e(String.valueOf(vertices[2][0]), String.valueOf(vertices[2][1]));
--- app/src/main/java/com/carlisle/ben/tanks/Tank.java
+++ app/src/main/java/com/carlisle/ben/tanks/Tank.java
@@ -11,9 +11,6 @@ public class Tank extends Entity {
     private int lives;
     private static double timer = 0.1;
     private int width;
-    private final static double LEFTVALUEDEC  = 0.3;
-    private final static double RIGHTVALUEINC = 0.5;
-    private final static double UPVALUEINC = 1.0;
     private final static double BULLETCONST = 10;
     private boolean isShootBottomPressed;
     private double xPercentage, yPercentage;
@@ -44,79 +41,68 @@ public class Tank extends Entity {
         this.yPercentage = yPercentage;
     }
 
-    public double getJoyAngle(){
-		double theta;
-		if (xPercentage != 0)
-			theta = (float) Math.atan(yPercentage/ xPercentage);
-		else
-			theta = 90;
-		if (xPercentage < 0)
-			theta += Math.PI;
-		return theta;
+    private int get_new_xPos(double xPercentage){
+        return (int) (getXpos() + speed*xPercentage/Math.sqrt(xPercentage*xPercentage + yPercentage* yPercentage) + 1/2 * (Math.pow(xPercentage,2)));
     }
 
-    private int get_new_xPos(double angle){
-        return (int) (getXpos() + speed*Math.cos(angle) + 1/2 * (Math.pow(xPercentage,2)));
-    }
-
-    private int get_new_yPos(double angle){
-        return (int)(getYpos() + speed*Math.sin(angle) + 1/2 * (Math.pow(yPercentage,2)));
+    private int get_new_yPos(double yPercentage){
+        return (int)(getYpos() + speed*yPercentage/Math.sqrt(xPercentage*xPercentage + yPercentage* yPercentage) + 1/2 * (Math.pow(yPercentage,2)));
     }
 
     public Bullet fire(){
-        return new Bullet((int)(getXpos() + BULLETCONST * Math.cos (getJoyAngle())),(int)(getYpos() + BULLETCONST * Math.cos (getJoyAngle())), getJoyAngle());
+        return new Bullet((int)(getXpos() + BULLETCONST * xPercentage),(int)(getYpos() + BULLETCONST * yPercentage), xPercentage, yPercentage);
     }
 
     public void move(float xPercentage, float yPercentage, Map map){
-
         this.xPercentage = xPercentage;
         this.yPercentage = yPercentage;
-		double angle = getJoyAngle();
-        int new_xPos = get_new_xPos(angle);
-        int new_yPos = get_new_yPos(angle);
-		map.moveEntity(getXpos(), getYpos(), (int)(getXpos() + speed*Math.cos(angle)), (int) (getYpos() + speed*Math.sin(angle)));
-		setPosition((int)(getXpos() + speed*Math.cos(angle)), (int)(getYpos() + speed*Math.sin(angle)));
-//		Log.e(String.valueOf(xPercentage), String.valueOf(yPercentage));
-//		Log.e(String.valueOf(angle),String.valueOf(angle));
-//		Log.e(String.valueOf(getXpos()), String.valueOf(getYpos()));
-//		Log.e(String.valueOf(new_xPos), String.valueOf(new_yPos));
+        speed = (int) (Math.sqrt(xPercentage*xPercentage + yPercentage*yPercentage)*10);
+        int new_xPos = get_new_xPos(xPercentage);
+        int new_yPos = get_new_yPos(yPercentage);
+		map.moveEntity(getXpos(), getYpos(), new_xPos, new_yPos);
+		setPosition(new_xPos, new_yPos);
+		Log.e("beg", "beg");
+		Log.e(String.valueOf(getXpos()), String.valueOf(getYpos()));
+		Log.e(String.valueOf(new_xPos), String.valueOf(new_yPos));
+		Log.e("fin", "fin");
 
 
-		if (true) {
-			return;
-		}
-        ArrayList<Entity> hittingEntities = new ArrayList<>();
-		int[] newVertex = new int[2];
-		for(int i = 0; i < Math.PI/2; i+= Math.PI/6) {
-            new_xPos = get_new_xPos(angle + i);
-            new_yPos = get_new_yPos(angle + i);
-            hittingEntities = getObjectsWithinRadius(new_xPos, new_yPos);
-            for (Entity entity: hittingEntities) {
-                if (entity != null) {
-					newVertex[0] = entity.getXpos();
-					newVertex[1] = entity.getYpos();
-                    if (!(new CollisionDetectorWithTank(getVertices(new_xPos, new_yPos, getJoyAngle() + i), newVertex).isCollision())) {
-                    	angle = angle + i;
-						map.moveEntity(getXpos(), getYpos(), (int)(getXpos() + speed*Math.cos(angle)), (int) (getYpos() + speed*Math.sin(angle)));
-						setPosition((int)(getXpos() + speed*Math.cos(angle)), (int)(getYpos() + speed*Math.sin(angle)));
-					}
-                }
-            }
-			new_xPos = get_new_xPos(angle - i);
-			new_yPos = get_new_yPos(angle - i);
-			hittingEntities = getObjectsWithinRadius(new_xPos, new_yPos);
-			for (Entity entity: hittingEntities) {
-				if (entity != null) {
-					newVertex[0] = entity.getXpos();
-					newVertex[1] = entity.getYpos();
-					if (!(new CollisionDetectorWithTank(getVertices(new_xPos, new_yPos, getJoyAngle() - i), newVertex).isCollision())) {
-						angle = angle - i;
-						map.moveEntity(getXpos(), getYpos(), (int)(getXpos() + speed*Math.cos(angle)), (int) (getYpos() + speed*Math.sin(angle)));
-						setPosition((int)(getXpos() + speed*Math.cos(angle)), (int)(getYpos() + speed*Math.sin(angle)));
-					}
-				}
-			}
-        }
+//		if (true) {
+//			return;
+//		}
+//        ArrayList<Entity> hittingEntities = new ArrayList<>();
+//		int[] newVertex = new int[2];
+//		double angle = 9;
+//		for(int i = 0; i < Math.PI/2; i+= Math.PI/6) {
+//            new_xPos = get_new_xPos(angle + i);
+//            new_yPos = get_new_yPos(angle + i);
+//            hittingEntities = getObjectsWithinRadius(new_xPos, new_yPos);
+//            for (Entity entity: hittingEntities) {
+//                if (entity != null) {
+//					newVertex[0] = entity.getXpos();
+//					newVertex[1] = entity.getYpos();
+//                    if (!(new CollisionDetectorWithTank(getVertices(new_xPos, new_yPos, getJoyAngle() + i), newVertex).isCollision())) {
+//                    	angle = angle + i;
+//						map.moveEntity(getXpos(), getYpos(), (int)(getXpos() + speed*Math.cos(angle)), (int) (getYpos() + speed*Math.sin(angle)));
+//						setPosition((int)(getXpos() + speed*Math.cos(angle)), (int)(getYpos() + speed*Math.sin(angle)));
+//					}
+//                }
+//            }
+//			new_xPos = get_new_xPos(angle - i);
+//			new_yPos = get_new_yPos(angle - i);
+//			hittingEntities = getObjectsWithinRadius(new_xPos, new_yPos);
+//			for (Entity entity: hittingEntities) {
+//				if (entity != null) {
+//					newVertex[0] = entity.getXpos();
+//					newVertex[1] = entity.getYpos();
+//					if (!(new CollisionDetectorWithTank(getVertices(new_xPos, new_yPos, getJoyAngle() - i), newVertex).isCollision())) {
+//						angle = angle - i;
+//						map.moveEntity(getXpos(), getYpos(), (int)(getXpos() + speed*Math.cos(angle)), (int) (getYpos() + speed*Math.sin(angle)));
+//						setPosition((int)(getXpos() + speed*Math.cos(angle)), (int)(getYpos() + speed*Math.sin(angle)));
+//					}
+//				}
+//			}
+//        }
     }
 
 	private ArrayList<Entity> getObjectsWithinRadius(int new_xPos, int new_yPos) {
@@ -125,7 +111,7 @@ public class Tank extends Entity {
 	}
 
 
-	public int [][]  getVertices(int newXPos, int newYPos, double newAngle){
+	public int [][]  getVertices(int newXPos, int newYPos){
 
         int [][] vertices = {{(int) (newXPos + width),(int) newYPos},
                 { (int) (newXPos + 0.3 * width), (int) (newYPos - 0.5 * width)},
