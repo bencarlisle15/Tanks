b3d7f9c7c5e2afd2e44df66f38f9f3f936f63b36
bencarlisle15 <bencarlisle15@gmail.com>
2018-04-06 21:24:42 -0400

    Added beginning of Map

--- app/src/main/java/com/carlisle/ben/tanks/Map.java
+++ app/src/main/java/com/carlisle/ben/tanks/Map.java
@@ -1,5 +1,41 @@
 package com.carlisle.ben.tanks;
 
-public class Map {
+import android.graphics.Bitmap;
+import android.media.Image;
 
+public class Map {
+	private Entity[][] entities;
+
+	public Map(Bitmap image) {
+		entities = new Entity[image.getHeight()][image.getWidth()];
+		for (int r = 0; r < image.getHeight(); r++) {
+			for (int c = 0; c < image.getWidth(); c++) {
+				if (isWall(image.getPixel(c, r))) {
+					entities[r][c] = new Wall(r, c);
+				} else {
+					entities[r][c] = null;
+				}
+			}
+		}
+	}
+
+	public Entity getEntity(int r, int c) {
+		return entities[r][c];
+	}
+
+	public void setEntity(int r, int c, Entity entity) {
+		entities[r][c] = entity;
+	}
+
+	public void moveEntity(int r1, int c1, int r2, int c2) {
+		entities[r2][c2] = entities[r1][c2];
+	}
+
+	private boolean isWall(int color) {
+		int a = (color >> 24) & 0xff;
+		int r = (color >> 16) & 0xff;
+		int g = (color >>  8) & 0xff;
+		int b = (color) & 0xff;
+		return a > 120 && r > 100 && r < 150 && g > 100 && g < 150 && b > 100 && b < 150;
+	}
 }
