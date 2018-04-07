50e1257fd6ff2391281a1907950249892a90f495
bencarlisle15 <bencarlisle15@gmail.com>
2018-04-07 13:30:49 -0400

    Edited bullet collision mechanics

--- app/src/main/java/com/carlisle/ben/tanks/Bullet.java
+++ app/src/main/java/com/carlisle/ben/tanks/Bullet.java
@@ -31,7 +31,8 @@ public class Bullet extends Entity {
 		int nextXPos = (int) (getXpos() + speed*Math.cos(angle));
 		int nextYPos = (int) (getYpos() + speed*Math.sin(angle));
         if (!collisionDetected(theMap, nextXPos, nextYPos)) {
-            setPosition((int) (getXpos() + speed*Math.cos(angle)), (int) (getYpos() + angle* sin(angle)));
+        	theMap.moveEntity(getXpos(), getYpos(), (int)(getXpos() + speed*Math.cos(angle)), (int) (getYpos() + speed*Math.sin(angle)));
+            setPosition((int)(getXpos() + speed*Math.cos(angle)), (int)(getYpos() + speed*Math.sin(angle)));
         } else {
 			nextXPos -= 2*Math.cos(angle);
 			nextYPos -= 2*Math.sin(angle);
@@ -40,6 +41,58 @@ public class Bullet extends Entity {
         		nextYPos -= i*Math.sin(angle);
 			}
 			//find the slope from the first value that is not the wall
+			int slopeX1;
+        	int slopeY1;
+        	int slopeX2;
+        	int slopeY2;
+        	boolean isLeft1 = false, isLeft2 = false, isRight1 = false, isRight2 = false;
+			for (int i  = 0; i < 5; i++) {
+        		if (!collisionDetected(theMap, nextXPos + i, nextYPos + 1)) {
+        			if (isRight1) {
+						slopeX1 = nextXPos + i;
+						slopeY1 = nextYPos + 1;
+						break;
+					}
+				} else if (!isRight1){
+        			isRight1 = true;
+				}
+				if (!collisionDetected(theMap, nextXPos - i, nextYPos + 1)) {
+					if (isLeft1) {
+						slopeX1 = nextXPos - i;
+						slopeY1 = nextYPos + 1;
+						break;
+					}
+				} else if (!isLeft1){
+					isLeft1 = true;
+				}
+				if (!collisionDetected(theMap, nextXPos + i, nextYPos - 1)) {
+					if (isRight2) {
+						slopeX2 = nextXPos + i;
+						slopeY2 = nextYPos - 1;
+						break;
+					}
+				} else if (!isRight2){
+					isRight2 = true;
+				}
+				if (!collisionDetected(theMap, nextXPos - i, nextYPos - 1)) {
+					if (isLeft2) {
+						slopeX2 = nextXPos - i;
+						slopeY2 = nextYPos - 1;
+						break;
+					}
+				} else if (!isLeft2){
+					isLeft2 = true;
+				}
+			}
+			if (isRight1 && isLeft2) {
+				//TODO
+			} else if (isRight2 && isLeft1) {
+				//TODO
+			} else {
+				angle *= -1;
+				theMap.moveEntity(getXpos(), getYpos(), (int)(getXpos() + speed*Math.cos(angle)), (int) (getYpos() + speed*Math.sin(angle)));
+				setPosition((int)(getXpos() + speed*Math.cos(angle)), (int)(getYpos() + speed*Math.sin(angle)));
+			}
         }
     }
 
